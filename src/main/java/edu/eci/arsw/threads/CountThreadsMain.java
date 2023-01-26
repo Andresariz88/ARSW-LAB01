02eb8415dcd281fef9eeda21ed2b45be9d57d04f
Andrés Ariza <andres-ariza70@hotmail.com>
2023-01-26 08:32:32 -0500

    Clase CountThreadMain

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,12 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+        Thread myThread0 = new Thread(new CountThread(0, 99));
+        Thread myThread1 = new Thread(new CountThread(99, 199));
+        Thread myThread2 = new Thread(new CountThread(199, 299));
+        myThread0.start();
+        myThread1.start();
+        myThread2.start();
     }
     
 }
