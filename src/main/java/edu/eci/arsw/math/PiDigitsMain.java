c5bde613140aadc6fe0d773b4b6ff8c5ec00b6fb
Juan Pablo <juanpasan2000@gmail.com>
2023-01-26 16:03:17 -0500

    Se corrigió logica del numero de threads

--- src/main/java/edu/eci/arsw/math/PiDigitsMain.java
+++ src/main/java/edu/eci/arsw/math/PiDigitsMain.java
@@ -1,30 +1,40 @@
 package edu.eci.arsw.math;
 
+import java.util.ArrayList;
+
 public class PiDigitsMain {
 
     public static void main(String args[]) {
         int start = 0;
-        int count = 10;
-        int n = 2;
+        int count = 50000;
+        int n = 1;
+        ArrayList<Thread> threads = new ArrayList<>();
 
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < n; i++) {
-            Thread t = new Thread(new PiDigits(i*(count/n), count));
+            Thread t;
+            if (i == n-1) {
+                t = new Thread(new PiDigits(i*(count/n), (count/n) + (count%n)));
+            } else {
+                t = new Thread(new PiDigits(i*(count/n), count/n));
+            }
+            threads.add(t);
             t.start();
+            /*try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }*/
+        }
+        for(Thread t:threads){
             try {
                 t.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-        /*Thread t = new Thread(new PiDigits(0, 5));
-        t.start();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        t = new Thread(new PiDigits(5, 5));
-        t.start();*/
+        System.out.println();
+        System.out.println(System.currentTimeMillis() - startTime);
 
     }
 }
